264442e3eb1918e1863e63863241e1c7ec22c858
NicolasBiondini <nicolasbiondini29@gmail.com>
2023-05-03 13:56:52 -0300

    finished v1.3.4

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,7 +16,7 @@ export default function Document() {
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'GA_MEASUREMENT_ID');
+          gtag('config', 'G-5QREECFYBC');
         `}
         </Script>
         <Main />
