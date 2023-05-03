2675c70b360bc7a6198949d1dbfb45ca685a57bd
NicolasBiondini <nicolasbiondini29@gmail.com>
2023-05-03 13:54:07 -0300

    finished v1.3.3

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,24 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en" className={`overflow-x-hidden `}>
       <Head></Head>
       <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-5QREECFYBC"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'GA_MEASUREMENT_ID');
+        `}
+        </Script>
         <Main />
         <NextScript />
       </body>
